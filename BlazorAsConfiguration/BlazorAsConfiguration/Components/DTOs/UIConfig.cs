022cd49568baf5019e01dec7322c3f95a7ea7d12
Christian Peters <christian.peters@gmail.com>
2025-07-29 17:43:45 -0500

    Start of component layer

---
+++ BlazorAsConfiguration/BlazorAsConfiguration/Components/DTOs/UIConfig.cs
@@ -0,0 +1,8 @@
+namespace BlazorAsConfiguration.Components.DTOs;
+
+public class UIConfig
+{
+    public string Type { get; set; }
+    public string Data { get; set; }
+    public Action? Action { get; set; }
+}
