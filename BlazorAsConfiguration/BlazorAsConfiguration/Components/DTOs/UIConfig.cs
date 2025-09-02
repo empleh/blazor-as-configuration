c205ab1262e58156e1ae3583e0fe7792a0dc52d7
Christian Peters <christian.peters@gmail.com>
2025-09-02 15:42:01 -0500

    dynamic data

--- BlazorAsConfiguration/BlazorAsConfiguration/Components/DTOs/UIConfig.cs
+++ BlazorAsConfiguration/BlazorAsConfiguration/Components/DTOs/UIConfig.cs
@@ -4,5 +4,6 @@ public class UIConfig
 {
     public string Type { get; set; }
     public string Data { get; set; }
+    public Func<string>? DynamicData { get; set; }
     public Action? Action { get; set; }
 }
